94402e5b7689f2fcdd33758fb88e02e1607ee9d6
chichi5454 <140154706+chichi5454@users.noreply.github.com>
2023-09-01 19:49:39 +0300

    added humburger menu for small screen

---
+++ app.js
@@ -0,0 +1,8 @@
+/** @format */
+
+function toogleMenu() {
+  const menu = document.querySelector(".hamburger-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
